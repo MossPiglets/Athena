d159e83c95eec03d50e4534d4425c3b019fa8870
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-01-02 20:04:57 +0100

    First changes

--- Athena/Athena/MainWindow.xaml.cs
+++ Athena/Athena/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Athena
+﻿using System.Collections.Generic;
+
+namespace Athena
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +10,27 @@
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            List<Data.Book> Books = new List<Data.Book>();
+
+            var book1 = new Data.Book();
+            book1.Title = "tytuł";
+
+            Books.Add(book1);
+           
+            bookList.ItemsSource = Books;
         }
+
+        private void Click_Edytuj_Książkę(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void Click_Dodaj_Książkę(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
+
