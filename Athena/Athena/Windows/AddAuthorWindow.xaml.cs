948a8b2c066fece18faed3f3f3ab1cca85cdf0e9
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-01-08 11:37:33 +0100

    Add author form

---
+++ Athena/Athena/Windows/AddAuthorWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Athena.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddAuthorWindow.xaml
+    /// </summary>
+    public partial class AddAuthorWindow
+    {
+        public AddAuthorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
