709898a97986a45e41dc68c6b40ff6e058980faa
Zofia Szybalska <zochula111@gmail.com>
2021-02-05 15:28:43 +0100

    Series Name Validation

--- Athena/Athena/Data/Series.cs
+++ Athena/Athena/Data/Series.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace Athena.Data {
-    public class Series {
+    public class Series : IDataErrorInfo
+    {
         public Guid Id { get; set; }
         public string SeriesName { get; set; }
         public int VolumeNumber { get; set; }
 
         public virtual ICollection<Book> Books { get; set; } = new ObservableCollection<Book>();
+
+        public string Error
+        {
+            get { return null; }
+        }
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = string.Empty;
+                if (columnName == "SeriesName")
+                {
+                    if (this.SeriesName == "")
+                        result = "Nazwa serii nie może być pusta.";
+                }
+                return result;
+            }
+        }
     }
 }
