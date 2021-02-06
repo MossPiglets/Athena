c585f4ba6fed22cd3a6fdf40f2a01b31c2e753a5
Zofia Szybalska <zochula111@gmail.com>
2021-02-06 17:24:25 +0100

    Add SeriesView

--- Athena/Athena/Data/Series.cs
+++ Athena/Athena/Data/Series.cs
@@ -4,30 +4,12 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 
 namespace Athena.Data {
-    public class Series : IDataErrorInfo
+    public class Series 
     {
         public Guid Id { get; set; }
         public string SeriesName { get; set; }
         public int VolumeNumber { get; set; }
 
         public virtual ICollection<Book> Books { get; set; } = new ObservableCollection<Book>();
-
-        public string Error
-        {
-            get { return null; }
-        }
-        public string this[string columnName]
-        {
-            get
-            {
-                string result = string.Empty;
-                if (columnName == "SeriesName")
-                {
-                    if (this.SeriesName == "")
-                        result = "Nazwa serii nie może być pusta.";
-                }
-                return result;
-            }
-        }
     }
 }
---
+++ Athena/Athena/Data/SeriesView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace Athena.Data
+{
+    class SeriesView : IDataErrorInfo
+    {
+        public Guid Id { get; set; }
+        public string SeriesName { get; set; }
+        public int VolumeNumber { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; } = new ObservableCollection<Book>();
+
+        public string Error
+        {
+            get { return null; }
+        }
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = string.Empty;
+                if (columnName == "SeriesName")
+                {
+                    if (this.SeriesName == "")
+                        result = "Nazwa serii nie może być pusta.";
+                }
+                return result;
+            }
+        }
+
+        public Series ToSeries()
+        {
+            
+            return null;
+        }
+    }
+}
