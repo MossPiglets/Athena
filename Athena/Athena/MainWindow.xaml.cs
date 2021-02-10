553d16a136f21009f9253ae2ebd5e8e27cee71e8
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-02-10 20:18:33 +0100

    BackgroundWorker

--- Athena/Athena/MainWindow.xaml.cs
+++ Athena/Athena/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
+using System;
 using Athena.Data;
 using Athena.Windows;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Windows;
 using Athena.Import;
@@ -11,31 +13,29 @@ using Microsoft.Win32;
 
 
 namespace Athena {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow {
-		public MainWindow() {
-			InitializeComponent();
-			this.DataContext = this;
-			BookList.ItemsSource = new List<Book>();
-			if (!BookList.ItemsSource.IsNullOrEmpty()) {
-				ImportButton.Visibility = Visibility.Hidden;
-			}
-		}
-
-        private void AddBook_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-			AddBookWindow addBook = new AddBookWindow();
-			addBook.Show();
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+            this.DataContext = this;
+            BookList.ItemsSource = new List<Book>();
+            if (!BookList.ItemsSource.IsNullOrEmpty()) {
+                ImportButton.Visibility = Visibility.Hidden;
+            }
         }
 
-        private void MenuItemEdit_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-			Book book = (Book)BookList.SelectedItem;
-			EditBookWindow editBook = new EditBookWindow(book); 
-			editBook.Show();
-		}
+        private void AddBook_Click(object sender, System.Windows.RoutedEventArgs e) {
+            AddBookWindow addBook = new AddBookWindow();
+            addBook.Show();
+        }
+
+        private void MenuItemEdit_Click(object sender, System.Windows.RoutedEventArgs e) {
+            Book book = (Book) BookList.SelectedItem;
+            EditBookWindow editBook = new EditBookWindow(book);
+            editBook.Show();
+        }
 
         private void MenuItemDelete_Click(object sender, System.Windows.RoutedEventArgs e) { }
 
@@ -46,10 +46,27 @@ namespace Athena {
             if (fileName == "") {
                 return;
             }
+
             var dataImporter = new DatabaseImporter();
             dataImporter.ImportFromSpreadsheet(fileName);
 
             ImportButton.Visibility = Visibility.Hidden;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e) {
+            BackgroundWorker worker = new BackgroundWorker { WorkerReportsProgress = true };
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            ProgressBarStatus.Value = e.ProgressPercentage;
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e) {
+            throw new NotImplementedException();
         }
     }
 }
