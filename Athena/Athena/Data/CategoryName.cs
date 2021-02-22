a0d6414d3fbbea01a2fa0cbc6d671f1d3b132f89
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-02-22 19:36:14 +0100

    LocalizedDescriptionAttribute

--- Athena/Athena/Data/CategoryName.cs
+++ Athena/Athena/Data/CategoryName.cs
@@ -1,41 +1,103 @@
-﻿using Athena.Resources;
+﻿using System.ComponentModel;
+using Athena.Resources;
 
-namespace Athena.Data
-{
-    public enum CategoryName
-    {
+namespace Athena.Data {
+    public enum CategoryName {
         [LocalizedDescription("Album", typeof(Categories))]
         Album,
+
+        [LocalizedDescription("Atlas", typeof(Categories))]
         Atlas,
+
+        [LocalizedDescription("Audiobook", typeof(Categories))]
         Audiobook,
+
+        [Description ("Pluuuuuuuuuuum")]
+        //[LocalizedDescription("Biography", typeof(Categories))]
         Biography,
+
+        [LocalizedDescription("Economy", typeof(Categories))]
         Economy,
+
+        [LocalizedDescription("ForChildren", typeof(Categories))]
         ForChildren,
+
+        [LocalizedDescription("YoungAdult", typeof(Categories))]
         YoungAdult,
+
+        [LocalizedDescription("Fantasy", typeof(Categories))]
         Fantasy,
+
+        [LocalizedDescription("Philosophy", typeof(Categories))]
         Philosophy,
+
+        [LocalizedDescription("History", typeof(Categories))]
         History,
+
+        [LocalizedDescription("Informatics", typeof(Categories))]
         Informatics,
+
+        [LocalizedDescription("Linguistics", typeof(Categories))]
         Linguistics,
+
+        [LocalizedDescription("Classic", typeof(Categories))]
         Classic,
+
+        [LocalizedDescription("Comic", typeof(Categories))]
         Comic,
+
+        [LocalizedDescription("Culinary", typeof(Categories))]
         Culinary,
+
+        [LocalizedDescription("NonFiction", typeof(Categories))]
         NonFiction,
+
+        [LocalizedDescription("LanguageLearning", typeof(Categories))]
         LanguageLearning,
+
+        [LocalizedDescription("Science", typeof(Categories))]
         Science,
+
+        [LocalizedDescription("TextBook", typeof(Categories))]
         TextBook,
+
+        [LocalizedDescription("Poetry", typeof(Categories))]
         Poetry,
+
+        [LocalizedDescription("Guide", typeof(Categories))]
         Guide,
+
+        [LocalizedDescription("Law", typeof(Categories))]
         Law,
+
+        [LocalizedDescription("Adventure", typeof(Categories))]
         Adventure,
+
+        [LocalizedDescription("Religion", typeof(Categories))]
         Religion,
+
+        [LocalizedDescription("Romance", typeof(Categories))]
         Romance,
+
+        [LocalizedDescription("PersonalDevelopment", typeof(Categories))]
         PersonalDevelopment,
+
+        [LocalizedDescription("Thriller", typeof(Categories))]
         Thriller,
+
+        [LocalizedDescription("Sociology", typeof(Categories))]
         Sociology,
+
+        [LocalizedDescription("Recreation", typeof(Categories))]
         Recreation,
+
+        [LocalizedDescription("Art", typeof(Categories))]
         Art,
+
+        [LocalizedDescription("Tourism", typeof(Categories))]
         Tourism,
+
+        [LocalizedDescription("Relationship", typeof(Categories))]
         Relationship
     }
 }
--- Athena/Athena/Resources/LocalizedDescriptionAttribute.cs
+++ Athena/Athena/Resources/LocalizedDescriptionAttribute.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Resources;
-using System.Text;
 
 namespace Athena.Resources {
     public class LocalizedDescriptionAttribute : DescriptionAttribute {
