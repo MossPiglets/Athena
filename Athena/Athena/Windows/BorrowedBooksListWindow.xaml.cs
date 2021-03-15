a311eb0519b1d8f3d91e5733ad1246094c4320c3
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-03-15 17:20:23 +0100

    Binding

--- Athena/Athena/Windows/BorrowedBooksListWindow.xaml.cs
+++ Athena/Athena/Windows/BorrowedBooksListWindow.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace Athena.Windows
+﻿using System.Collections.ObjectModel;
+using Athena.Data.Borrowings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Athena.Windows
 {
     public partial class BorrowedBooksListWindow
     {
-        public BorrowedBooksListWindow()
-        {
+        private ApplicationDbContext ApplicationDbContext { get; set; }
+        public ObservableCollection<Borrowing> Borrowings { get; set; }
+        public BorrowedBooksListWindow() {
             InitializeComponent();
+            this.DataContext = this;
+            ApplicationDbContext = new ApplicationDbContext();
+            ApplicationDbContext.Borrowings
+                .Include(a => a.Book)
+                .Include(a => a.Book.StoragePlace)
+                .Include(a => a.Book.PublishingHouse)
+                .Include(a => a.Book.Authors)
+                .Load();
+            Borrowings = ApplicationDbContext.Borrowings.Local.ToObservableCollection();
+            BorrowedBookList.ItemsSource = Borrowings;
         }
     }
 }
