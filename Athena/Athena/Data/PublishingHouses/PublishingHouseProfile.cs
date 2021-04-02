6546743c80300eb90f03eab7cc4788e1a5f13b34
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-04-02 20:14:37 +0200

    AddSomeBadBinding

---
+++ Athena/Athena/Data/PublishingHouses/PublishingHouseProfile.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Athena.Data.PublishingHouses {
+    public class PublishingHouseProfile : Profile {
+        public PublishingHouseProfile() {
+            CreateMap<PublishingHouse, PublishingHouseView>().ReverseMap();
+        }
+    }
+}
---
+++ Athena/Athena/Data/PublishingHouses/PublishingHouseView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Athena.Data.Books;
+
+namespace Athena.Data.PublishingHouses {
+    public class PublishingHouseView {
+        public Guid Id { get; set; }
+        public string PublisherName { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
