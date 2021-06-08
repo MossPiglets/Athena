5b15ed080401d2680f6554afc3749f87e003b323
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-06-08 15:40:28 +0200

    Added new converter

---
+++ Athena/Athena/Converters/ZeroToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using Castle.Core.Internal;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Athena.Converters
+{
+    class ZeroToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value == null) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    {
+    }
+}
